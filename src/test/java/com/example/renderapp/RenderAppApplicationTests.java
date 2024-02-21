1bc9d029dfe1c51d81faf756ae13834ef7712bb8
Zayniddin <rikhsiboyevzaxa@gmail.com>
2024-02-21 11:56:37 +0500

    +

---
+++ src/test/java/com/example/renderapp/RenderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
